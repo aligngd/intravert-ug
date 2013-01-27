6544dd9256facc791d4a17ce1333464c5534b5ec
Edward Capriolo <edlinuxguru@gmail.com>
2013-01-26 20:28:35 -0500

    More scanner stuff

---
+++ src/test/java/org/usergrid/vx/experimental/scan/PeopleFromNY.java
@@ -0,0 +1,36 @@
+package org.usergrid.vx.experimental.scan;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.cassandra.utils.ByteBufferUtil;
+import org.usergrid.vx.experimental.IntraService;
+import org.usergrid.vx.experimental.IntraState;
+import org.vertx.java.core.Vertx;
+
+public class PeopleFromNY implements ScanFilter {
+
+	public PeopleFromNY() {
+	}
+
+	@Override
+	public boolean filter(Map currentRow, ScanContext c) {
+		String val = null;
+		try {
+			val = ByteBufferUtil.string(((ByteBuffer) currentRow.get("value")));
+		} catch (CharacterCodingException e) {
+			
+		}
+		if (val.compareTo("NY")==1){
+			return false; //stop if we get past ny
+		} else if (val.equals("NY")){
+			c.collect(currentRow);
+			return true;
+		} else {
+			return true;
+		}
+	}
+
+}
